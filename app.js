ef5f2d94fb22b4a970a45cda8fc73c31c6742565
satpal82bhandari <satpal82bhandari@gmail.com>
2016-07-11 12:38:40 +0530

    asda

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var https = require('https');
+var http = require('http');
 var bodyParser = require('body-parser');
 var app = express();
 var pokerbot = require('./pokerbot');
 
 
-var port = process.argv[2] ? process.argv[2] : 3000;
+var port = process.env.PORT ? process.env.PORT : 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var hskey = fs.readFileSync('key.pem');
-var hscert = fs.readFileSync('cert.pem');
-
-var options = {
-    key: hskey,
-    cert: hscert
-};
-
-https.createServer(options, app).listen(port);
+http.createServer(app).listen(port);
 
 app.get('/', pokerbot.root);
