7f021b4f3e47ff63425af1307f7e0363c49937c3
satpal82bhandari <satpal82bhandari@gmail.com>
2016-07-12 12:31:28 +0530

    asdads

--- pokerbot.js
+++ pokerbot.js
@@ -1,10 +1,11 @@
 var pokerbot = {};
 
 pokerbot.root = function (req, res, next) {
-  console.log(req);
+  console.log(req.connection.remoteAddress);
   var botPayload = {
     text : 'Hello, World!'
   };
+  console.log(res.statusCode);
   return res.status(200).json(botPayload);
 }
 
