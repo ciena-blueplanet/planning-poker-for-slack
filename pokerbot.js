b520c91f7de63464f6c767836b6a6a4b3d11195f
satpal82bhandari <satpal82bhandari@gmail.com>
2016-07-13 09:22:52 +0530

    asdads

--- pokerbot.js
+++ pokerbot.js
@@ -6,6 +6,47 @@ pokerbot.root = function (req, res, next) {
     text : 'Hello, World!'
   };
   console.log(res.statusCode);
+
+  var  botPayload =
+      "text": "Would you like to play a game?",
+      "attachments": [
+          {
+              "text": "Choose a game to play",
+              "fallback": "You are unable to choose a game",
+              "callback_id": "wopr_game",
+              "color": "#3AA3E3",
+              "attachment_type": "default",
+              "actions": [
+                  {
+                      "name": "chess",
+                      "text": "Chess",
+                      "type": "button",
+                      "value": "chess"
+                  },
+                  {
+                      "name": "maze",
+                      "text": "Falken's Maze",
+                      "type": "button",
+                      "value": "maze"
+                  },
+                  {
+                      "name": "war",
+                      "text": "Thermonuclear War",
+                      "style": "danger",
+                      "type": "button",
+                      "value": "war",
+                      "confirm": {
+                          "title": "Are you sure?",
+                          "text": "Wouldn't you prefer a good game of chess?",
+                          "ok_text": "Yes",
+                          "dismiss_text": "No"
+                      }
+                  }
+              ]
+          }
+      ]
+  }
+
   return res.status(200).json(botPayload);
 }
 
