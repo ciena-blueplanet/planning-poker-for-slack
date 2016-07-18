8c965a8cf66a1098c7107ab58939e9a1673ccc53
satpal82bhandari <satpal82bhandari@gmail.com>
2016-07-18 12:06:14 +0530

    sfds

--- auth.js
+++ auth.js
@@ -8,8 +8,7 @@ auth.getToken = function (req, res, next) {
  var code =  req.query.code;
 
  var extServerOptions = {
-  host: 'https://slack.com/api/oauth.access',
-  path: '%3Fclient_id%3D55672736423.58936834261%26client_secret%3D831e9b5e514c9587d7f3820aac9b0299%26code='+code,
+  host: 'https://slack.com/api/oauth.access%3Fclient_id%3D55672736423.58936834261%26client_secret%3D831e9b5e514c9587d7f3820aac9b0299%26code='+code,
   method: 'GET'
  };
 
