2d8e14280c8bd2595c7111ee28760d4d582a4f11
satpal82bhandari <satpal82bhandari@gmail.com>
2016-07-18 12:46:00 +0530

    sfds

--- auth.js
+++ auth.js
@@ -6,15 +6,14 @@ auth.getToken = function (req, res, next) {
 
  console.log(req.params);
  var code =  req.query.code;
- var encodedPath = encodeURIComponent('/api/oauth.access?client_id=55672736423.58936834261&redirect_uri=https://orchestraion-india.slack.com/messages/&client_secret=831e9b5e514c9587d7f3820aac9b0299&code=')+code;
 
  var extServerOptions = {
+
   hostname: 'slack.com',
-  path: encodedPath,
+  path: '/api/oauth.access?client_id=55672736423.58936834261&redirect_uri=https://orchestraion-india.slack.com/messages/&client_secret=831e9b5e514c9587d7f3820aac9b0299&code='+code,
   method: 'GET'
  };
-
- console.log(extServerOptions);
+ console.log('Request object from Client : '+extServerOptions);
  var req = https.request(extServerOptions, (res) => {
   res.on('data', (d) => {
    process.stdout.write(d);
