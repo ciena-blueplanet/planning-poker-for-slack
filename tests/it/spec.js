ec6431c70ee7fc801cfd3b49e6f2088f44932ded
satpal82bhandari <satpal82bhandari@gmail.com>
2016-07-24 21:01:00 +0530

    Adding pr-bump

--- tests/it/spec.js
+++ tests/it/spec.js
@@ -18,7 +18,6 @@ describe('GET for root route ', function () {
   it('responds to /', function testSlash (done) {
     request(url)
     .get('/')
-    .expect('Content-Type', 'text/html; charset=UTF-8')
     .end(function (err, res) {
       if (err) return done(err)
       should.equal(res.status, 200, 'Status code for / is not 200')
@@ -29,11 +28,67 @@ describe('GET for root route ', function () {
   it('404 everything else', function testPath (done) {
     request(url)
       .get('/foo/bar')
-      .expect('Content-Type', 'text/html; charset=utf-8')
       .end(function (err, res) {
         if (err) return done(err)
         should.equal(res.status, 404, 'Status code for unregistered resource is not 404')
         done()
       })
+  })
+
+  it('Start and stop playing.', function testSlash (done) {
+    let jiraId = Math.floor((Math.random() * 1000) + 1)
+    request(url)
+    .post('/start')
+    .send({
+      'text': 'start JIRA-' + jiraId
+    })
+    .end(function (err, res) {
+      if (err) return done(err)
+      should.equal(res.status, 200, 'Status code for /start is not 200')
+      should.equal(res.body.text, 'Please give your poker vote for JIRA-' + jiraId, 'Response body is not ok.')
+      request(url)
+      .post('/start')
+      .send({
+        'text': 'stop JIRA-' + jiraId
+      })
+      .end(function (err, res) {
+        if (err) return done(err)
+        should.equal(res.status, 200, 'Status code for /start is not 200')
+        should.equal(res.body.text, 'Planning for this JIRA ID is complete. Thanks for Playing.', 'Response not ok.')
+        done()
+      })
+    })
+  })
+
+  it('responds to /vote with start', function testSlash (done) {
+    let jiraId = 'JIRA-' + Math.floor((Math.random() * 1000) + 1)
+    const startPayload = require('../test-data.json').startPayload
+    startPayload.original_message.text = 'start ' + jiraId
+    let requestBody = {
+      payload: JSON.stringify(startPayload)
+    }
+    request(url)
+    .post('/vote')
+    .send(requestBody)
+    .end(function (err, res) {
+      if (err) return done(err)
+      should.equal(res.status, 200, 'Status code for /start is not 200')
+      should.equal(res.body.text, 'You have voted 8 for JIRA ID : ' + jiraId.split('JIRA-')[1], 'Response not ok.')
+      done()
+    })
+  })
+
+  it('responds to /start with wrong format', function testSlash (done) {
+    const errorMessage = 'Response not ok.'
+    const successMessage = 'Please enter the command in correct format e.g. /planning-poker start or stop JIRA-1001'
+    request(url)
+    .post('/start')
+    .send({'text': 'wrongFormat'})
+    .end(function (err, res) {
+      if (err) return done(err)
+      should.equal(res.status, 200, 'Status code for /start is not 200')
+      should.equal(res.body.text, successMessage, errorMessage)
+      done()
+    })
   })
 })
